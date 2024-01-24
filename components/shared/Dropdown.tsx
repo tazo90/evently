f913aef4d5cb99180f5acf1f35eed41341c2a384
Mariusz Winnik <tazo90@gmail.com>
2024-01-25 00:55:57 +0100

    Add EventForm

---
+++ components/shared/Dropdown.tsx
@@ -0,0 +1,49 @@
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { ICategory } from "@/lib/database/models/category.model";
+import { useState } from "react";
+
+type DropdownProps = {
+  value?: string;
+  onChangeHandler?: () => void;
+};
+
+const Dropdown = ({ value, onChangeHandler }: DropdownProps) => {
+  const [categories, setCategories] = useState<ICategory[]>([
+    {
+      _id: "1",
+      name: "Category 1",
+    },
+    {
+      _id: "2",
+      name: "Category 2",
+    },
+  ]);
+
+  return (
+    <Select onValueChange={onChangeHandler} defaultValue={value}>
+      <SelectTrigger className="select-field">
+        <SelectValue placeholder="Category" />
+      </SelectTrigger>
+      <SelectContent>
+        {categories.length > 0 &&
+          categories.map((category) => (
+            <SelectItem
+              key={category._id}
+              value={category._id}
+              className="select-item p-regular-14"
+            >
+              {category.name}
+            </SelectItem>
+          ))}
+      </SelectContent>
+    </Select>
+  );
+};
+
+export default Dropdown;
