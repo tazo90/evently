de9305e2d3548f24d256f8308deb94264aa9dd12
Mariusz Winnik <tazo90@gmail.com>
2024-01-27 20:37:16 +0100

    Deleted confirmation

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -1,11 +1,16 @@
 "use server";
 
-import { CreateEventParams, GetAllEventsParams } from "@/types";
+import {
+  CreateEventParams,
+  DeleteEventParams,
+  GetAllEventsParams,
+} from "@/types";
 import { handleError } from "../utils";
 import { connectToDatabase } from "../database";
 import User from "../database/models/user.model";
 import Event from "../database/models/event.model";
 import Category from "../database/models/category.model";
+import { revalidatePath } from "next/cache";
 
 const populateEvent = async (query: any) => {
   return query
@@ -77,6 +82,17 @@ export async function getAllEvents({
       data: JSON.parse(JSON.stringify(events)),
       totalPages: Math.ceil(eventsCount / limit),
     };
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+export async function deleteEvent({ eventId, path }: DeleteEventParams) {
+  try {
+    await connectToDatabase();
+
+    const deletedEvent = await Event.findByIdAndDelete(eventId);
+    if (deletedEvent) revalidatePath(path);
   } catch (error) {
     handleError(error);
   }
