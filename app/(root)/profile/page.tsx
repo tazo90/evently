996c98fab1e93f8c51a2362f3972dd8db3258aca
Mariusz Winnik <tazo90@gmail.com>
2024-01-28 22:31:38 +0100

    Add search feature

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -3,18 +3,22 @@ import { Button } from "@/components/ui/button";
 import { getEventsByUser } from "@/lib/actions/event.actions";
 import { getOrdersByUser } from "@/lib/actions/order.actions";
 import { IOrder } from "@/lib/database/models/order.model";
+import { SearchParamProps } from "@/types";
 import { auth } from "@clerk/nextjs";
 import Link from "next/link";
 import React from "react";
 
-const ProfilePage = async () => {
+const ProfilePage = async ({ searchParams }: SearchParamProps) => {
   const { sessionClaims } = auth();
   const userId = sessionClaims?.userId as string;
 
-  const orders = await getOrdersByUser({ userId, page: 1 });
+  const ordersPage = Number(searchParams?.ordersPage) || 1;
+  const eventsPage = Number(searchParams?.eventsPage) || 1;
+
+  const orders = await getOrdersByUser({ userId, page: ordersPage });
   const orderedEvents = orders?.data.map((order: IOrder) => order.event || []);
 
-  const organizedEvents = await getEventsByUser({ userId, page: 1 });
+  const organizedEvents = await getEventsByUser({ userId, page: eventsPage });
 
   return (
     <>
@@ -35,8 +39,8 @@ const ProfilePage = async () => {
           emptyStateSubtext="No worries - plenty of exiting events to explore!"
           collectionType="My_Tickets"
           limit={3}
-          page={1}
-          totalPages={2}
+          page={ordersPage}
+          totalPages={orders?.totalPages}
           urlParamName="ordersPage"
         />
       </section>
@@ -58,8 +62,8 @@ const ProfilePage = async () => {
           emptyStateSubtext="Go create some now"
           collectionType="Events_Organized"
           limit={6}
-          page={1}
-          totalPages={2}
+          page={eventsPage}
+          totalPages={organizedEvents?.totalPages}
           urlParamName="eventsPage"
         />
       </section>
